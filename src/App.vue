dff1c5c52928201deb1daf4b3ddbd81525a3a4de
Ligia Gadotti <angelisgadotti@gmail.com>
2022-08-24 14:19:45 -0300

    v1

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script>
+import CalculadoraComponente from "@/components/CalculadoraComponente.vue";
+
+export default {
+  components: { CalculadoraComponente },
+};
+</script>
+<template>
+  <CalculadoraComponente />
+</template>
+<style></style>
---
+++ src/components/CalculadoraComponente.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from "vue";
+const pessoas = ref("");
+const preco = ref("");
+</script>
+<template>
+  <main>
+    <h1>Calculadora de Gorjetas</h1>
+
+    <label>Valor da Conta?</label>
+    <input type="number" v-model="preco" />
+    <label>Como foi o Serviço?</label>
+    <select name="Qualidade do serviço" v-model="gorjeta" id="asd">
+      <option value="5">5% (Péssimo)</option>
+      <option value="10">10% (Ruim)</option>
+      <option value="15">15% (Ok)</option>
+      <option value="20">20% (Bom)</option>
+      <option value="25">25% (Mais que Bom)</option>
+      <option value="30">30% (Perfeito)</option>
+    </select>
+    <label>Quantas pessoas irão dividir?</label>
+    <input type="number" v-model="pessoas" />
+    <h4>
+      Valor da conta: R${{ (preco * (1 + gorjeta / 100)) / pessoas }} para cada
+      pessoa
+    </h4>
+  </main>
+</template>
